004de3e3950c31f1a1d6c6056d80871c8cbb0159
David Gaspar <davidgaspar.dev@gmail.com>
2021-05-21 00:46:05 -0300

    TypeAdapter for InvoiceData created

---
+++ lib/app/database/hive_adapters.dart
@@ -0,0 +1,30 @@
+/// External package
+import 'package:hive/hive.dart' show TypeAdapter, BinaryReader, BinaryWriter;
+/// Internal package
+import 'package:mytickets/app/database/models/invoice_data.dart';
+
+final List<TypeAdapter<dynamic>> typeAdapters = [
+  _InvoiceTypeAdapter()
+];
+
+/// Invoice Type Adapater
+/// 
+/// Adding [InvoiceData] type in the Hive database
+class _InvoiceTypeAdapter extends TypeAdapter<InvoiceData> {
+
+  /// Type adapter id (reference)
+  @override
+  int get typeId => 0;
+
+  /// Reading from the database.
+  @override
+  InvoiceData read(BinaryReader binaryReader) {
+    return InvoiceData.fromJson(Map<String, dynamic>.from(binaryReader.read()));
+  }
+  
+  /// Writing in the database
+  @override
+  void write(BinaryWriter binaryWriter, InvoiceData data) {
+    binaryWriter.write(data.toJson());
+  }
+}
