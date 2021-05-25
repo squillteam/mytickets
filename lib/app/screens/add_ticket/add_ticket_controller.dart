d41970731e9a187e144a47f8ed7265b56c731693
David Gaspar <davidgaspar.dev@gmail.com>
2021-05-25 11:02:45 -0300

    Add Ticket Controller created

---
+++ lib/app/screens/add_ticket/add_ticket_controller.dart
@@ -0,0 +1,30 @@
+/// External package
+import 'package:flutter/material.dart';
+/// Internal package
+import 'package:mytickets/app/helpers/abstractions/controller.dart';
+
+/// Add Ticket Controller
+class AddTicketController extends Controller {
+
+  BuildContext? _context;
+
+  @override
+  set context(BuildContext context) {
+    // TODO: implement context
+  }
+
+  @override
+  void init() {
+    // TODO: implement init
+  } 
+
+  @override
+  void builded() {
+    // TODO: implement builded
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+}
