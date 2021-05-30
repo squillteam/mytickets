31841d16dd00f72a712668c3f5b1c83d9742325e
David Gaspar <davidgaspar.dev@gmail.com>
2021-05-30 18:18:29 -0300

    Color palette defined - close #10

---
+++ lib/app/helpers/palette.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+/// Palette 
+class Palette {
+
+  // Primary
+  static const Color yellowGreenCrayola = Color(0xFFB5E48C);
+  static const Color grannySmithApple = Color(0xFF99D98C);
+  static const Color oceanGreen = Color(0xFF76C893);
+  static const Color keppel = Color(0xFF52B69A);
+  static const Color viridianGreen = Color(0xFF34A0A4);
+  static const Color blueMunsell = Color(0xFF168AAD);
+
+  // Base
+  static const Color greyCultured = Color(0xFFEDEEEF);
+  static const Color greyGainsboro = Color(0xFFDEDFDF);
+  static const Color greySilverSand = Color(0xFFBFC0BF);
+  static const Color greyDavys = Color(0xFF4C4D4B);
+  static const Color greyJet = Color(0xFF313231);
+  static const Color greyEerieBlack = Color(0xFF161616);
+}
